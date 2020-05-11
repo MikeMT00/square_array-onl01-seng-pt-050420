b9e5b9119aafcf07e3d446f2e8c482898a0a15a4
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-11 01:15:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = [ ] #Pushing squared numbers into this argument
   array.each do |number| #Some type of method is passed to each number
     new_array << number**2 #number**2 squares number (the variable)
-
+  end
 end
 
 
