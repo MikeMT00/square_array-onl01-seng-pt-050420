98682d5b037170b00ce1cbe16983f993f1fcfedb
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-11 01:00:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   array = [1, 4, 9]
 end
+
+
+
+
+
+
+#primary_colors = ["Red", "Yellow", "Blue"]
+#primary_colors.each do |color|
+#  puts "Primary Color #{color} is #{color.length} letters long."
+#end
